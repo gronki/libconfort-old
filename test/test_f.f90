17338fe835afbecd6f8068d6e419451f664e9fba
gronki <gronki@gmail.com>
2016-10-24 14:38:49 +0200

    Testy do przepisanej wersji biblioteki

---
+++ test/test_f.f90
@@ -0,0 +1,136 @@
+program test
+
+    use miniconf
+    use iso_fortran_env
+    use iso_c_binding
+
+    integer :: failure
+
+    call test_nonexistent_file(failure)
+    call write_result('test_nonexistent_file',failure)
+
+    call test_nonexistent_key(failure)
+    call write_result('test_nonexistent_key',failure)
+
+    call test_get_existing_key(failure)
+    call write_result('test_get_existing_key',failure)
+
+    ! call test_get_existing_keydef(failure)
+    ! call write_result('test_get_existing_keydef',failure)
+
+
+contains
+
+    subroutine write_result(test,failure)
+        character(len=*), intent(in) :: test
+        character(len=12) :: str
+        integer, intent(in) :: failure
+        integer, save :: i = 1
+
+        if (failure .eq. 0) then
+            str = 'OK'
+        else
+            str='FAILED!'
+        endif
+
+        write (output_unit,'(I2,A30,A8)') i, test, trim(str)
+        i = i + 1
+    end subroutine
+
+
+    subroutine test_nonexistent_file(failure)
+        integer, intent(inout) :: failure
+        integer(c_int) :: errno
+        type(miniconf_c) :: cfg
+        failure = 0
+
+        call mincf_read_file(cfg, &
+                & "Tegoplikuniema" // char(0), &
+                & errno)
+
+        if (errno .eq. 0) then
+            failure = 1
+            call mincf_free(cfg)
+        end if
+    end subroutine
+
+
+    subroutine test_nonexistent_key(failure)
+        integer, intent(inout) :: failure
+        integer(c_int) :: errno
+        type(miniconf_c) :: cfg
+        character(len=150) :: buf
+        failure = 0
+
+        call mincf_read_file(cfg, &
+                & "test.cfg" // char(0), &
+                & errno)
+
+        if (errno .ne. MINCF_OK) then
+            failure = 1
+            return
+        end if
+
+        call mincf_get(cfg, &
+                & "blablablablabla" // char(0), &
+                & buf, len(buf,kind=c_size_t), errno)
+
+        if ( errno .ne. MINCF_NOT_FOUND )  &
+            & failure = 1
+
+        call mincf_free(cfg)
+    end subroutine
+
+
+    subroutine test_get_existing_key(failure)
+
+        integer, intent(inout) :: failure
+        integer(c_int) :: errno
+        type(miniconf_c) :: cfg
+        failure = 0
+
+        call mincf_read_file(cfg, &
+                & "test.cfg" // char(0), &
+                & errno)
+
+        if (errno .ne. MINCF_OK) then
+            failure = 1
+            return
+        end if
+
+        try : block
+
+            character(len=150) :: buf
+
+            call mincf_get(cfg, &
+                    & "key1" // char(0), &
+                    & buf, len(buf,kind=c_size_t), errno)
+
+            if ( errno .ne. MINCF_OK ) then
+                failure = 1
+                exit try
+            end if
+
+            call cstr_import(buf, len(buf,kind=c_size_t))
+
+            if ( buf .ne. "value1" ) &
+                & failure = 1
+
+        end block try
+
+        call mincf_free(cfg)
+
+
+    end subroutine
+
+
+    ! subroutine test_get_existing_keydef(failure)
+    !     integer, intent(inout) :: failure
+    !
+    !     failure = 0
+    !
+    ! end subroutine
+
+
+
+end program test
